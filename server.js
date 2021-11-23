ccb4c6195567ad71dd3e75206d9a8a424cc527dc
Paul John Gonzales <paulj_gonzales@hotmail.com>
2021-11-22 23:14:00 -0500

    Created profile and image routes.

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require('express');
+const bcrypt = require('bcrypt-nodejs');
+const cors = require('cors');
 const app = express();
 
 // Run middleware to be able to parse out JSON requests
 app.use(express.json());
+app.use(cors());
 
 const database = {
     users: [
@@ -30,19 +33,30 @@ app.get('/', (req, res) => {
 });
 
 app.post('/signin', (req, res) => {
+    // Load hash from your password DB.
+    bcrypt.compare("test", '$2a$10$FdjQ/X6QiVCItzJXcWPoqOvv0.ts77gjPAR0l8AI25axbhEyWGh.O', function(err, res) {
+        console.log('guess: ', res)
+    });
+    bcrypt.compare("veggies", '$2a$10$FdjQ/X6QiVCItzJXcWPoqOvv0.ts77gjPAR0l8AI25axbhEyWGh.O', function(err, res) {
+        console.log('2nd guess: ', res)
+    });
+
     // Check if the email and password matches up in our database (db does not exist yet)
     if(req.body.email === database.users[0].email
     && req.body.password === database.users[0].password) {
-        res.json("Login successful!");
+        res.json("success");
     } else {
         res.status(400).json("Login is incorrect!");
     }
-    res.json("SignIn");
 });
 
 app.post('/register', (req, res) => {
     // The request sends a JSON that contains email, name, password properties
     const {name, password, email} = req.body;
+
+    bcrypt.hash(password, null, null, function(err, hash) {
+        console.log(hash);
+    });
 
     // Add the new user to the database (db does not exist yet)
     database.users.push({
@@ -56,6 +70,43 @@ app.post('/register', (req, res) => {
     
     res.json(database.users[database.users.length-1]);
 });
+
+app.get('/profile/:id', (req, res) => {
+    const { id } = req.params;
+    let userFound = false;
+
+    database.users.forEach( user => {
+        if (user.id === id) {
+            userFound = true;
+            return res.json(user);
+        }
+    });
+
+    if (!userFound) {
+        res.status(400).json('User does not exist.');
+    }
+});
+
+app.put('/image', (req, res) => {
+    const { id } = req.body;
+    let userFound = false;
+
+    database.users.forEach( user => {
+        if (user.id === id) {
+            userFound = true;
+            user.entries++;
+            return res.json(user.entries);
+        }
+    });
+
+    if (!userFound) {
+        res.status(400).json('User does not exist.');
+    }
+});
+
+
+
+
 
 app.listen(3000, () => {
     console.log("Working on port 3000!");
