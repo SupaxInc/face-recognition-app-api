0003b1148a8949ece0e769a52b79eff80612f59c
Paul John Gonzales <paulj_gonzales@hotmail.com>
2021-11-23 23:38:23 -0500

    Removed unnecessary console logs and fixed register route

--- server.js
+++ server.js
@@ -33,14 +33,6 @@ app.get('/', (req, res) => {
 });
 
 app.post('/signin', (req, res) => {
-    // Load hash from your password DB.
-    bcrypt.compare("test", '$2a$10$FdjQ/X6QiVCItzJXcWPoqOvv0.ts77gjPAR0l8AI25axbhEyWGh.O', function(err, res) {
-        console.log('guess: ', res)
-    });
-    bcrypt.compare("veggies", '$2a$10$FdjQ/X6QiVCItzJXcWPoqOvv0.ts77gjPAR0l8AI25axbhEyWGh.O', function(err, res) {
-        console.log('2nd guess: ', res)
-    });
-
     // Check if the email and password matches up in our database (db does not exist yet)
     if(req.body.email === database.users[0].email
     && req.body.password === database.users[0].password) {
@@ -54,15 +46,10 @@ app.post('/register', (req, res) => {
     // The request sends a JSON that contains email, name, password properties
     const {name, password, email} = req.body;
 
-    bcrypt.hash(password, null, null, function(err, hash) {
-        console.log(hash);
-    });
-
     // Add the new user to the database (db does not exist yet)
     database.users.push({
         id: '125',
         name: name,
-        password: password,
         email: email,
         entries: 0,
         createdDate: new Date()
